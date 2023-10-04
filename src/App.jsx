bccb2543237bdaec9474b3854e5777bdcd6262af
psuip312 <psudip312@gmail.com>
2023-10-04 15:51:58 +0545

    Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ function App() {
   // In essence, this code is defining padding styles for HTML elements with the class .padding. On screens larger than or equal to "sm" size, these elements will have a horizontal padding of px-16 and a vertical padding of py-24. On screens smaller than "sm," they will have a horizontal padding of px-8 and a vertical padding of py-12.
   return (
     <>
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">
-        hellll
-      </section>
-      <section className="padding">Popular Products</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Serives</section>
-      <section className="padding">Special Offers</section>
-      <section className="padding">customer Revier</section>
-      <section className="padding">Subscribe</section>
-      <section className="padding">Footer</section>
+      <main className="relative">
+        Nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          hellll
+        </section>
+        <section className="padding">Popular Products</section>
+        <section className="padding">SuperQuality</section>
+        <section className="padding-x py-10">Serives</section>
+        <section className="padding">Special Offers</section>
+        <section className=" bg-pale-blue padding">customer Revier</section>
+        <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+        <section className="bg-black padding-x padding-t pb-8 padding text-white">
+          Footer
+        </section>
+      </main>
     </>
   );
 }
